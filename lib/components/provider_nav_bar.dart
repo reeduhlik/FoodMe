6279dd97f5a4d3f65066b3ff4ab29d6703e23a9f
cmeuse <chloemeuse@gmail.com>
2023-03-21 02:03:29 -0400

    Chloe - Personal profile framework

---
+++ lib/components/provider_nav_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../screens/provider_home/provider_home_screen.dart';
+import '../screens/personal_profile/profile_screen.dart';
+
+import '../constants.dart';
+import '../enums.dart';
+
+class ProviderNavBar extends StatelessWidget {
+  const ProviderNavBar({
+    Key? key,
+    required this.selectedMenu,
+  }) : super(key: key);
+
+  final ProviderMenuState selectedMenu;
+
+  @override
+Widget build(BuildContext context) {
+    final Color inActiveIconColor = Color(0xFFB6B6B6);
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 14),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -15),
+            blurRadius: 20,
+            color: Color(0xFFDADADA).withOpacity(0.15),
+          ),
+        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      child: SafeArea(
+          top: false,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: SvgPicture.asset(
+                  "assets/icons/Shop Icon.svg",
+                  color: BusinessMenuState.home == selectedMenu
+                      ? kPrimaryColor
+                      : inActiveIconColor,
+                ),
+                onPressed: () =>
+                    Navigator.pushNamed(context, ProviderHomeScreen.routeName),
+              ),
+              IconButton(
+                icon: SvgPicture.asset("assets/icons/Heart Icon.svg"),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: SvgPicture.asset("assets/icons/Chat bubble Icon.svg"),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: SvgPicture.asset(
+                  "assets/icons/User Icon.svg",
+                  color: BusinessMenuState.profile == selectedMenu
+                      ? kPrimaryColor
+                      : inActiveIconColor,
+                ),
+                onPressed: () =>
+                    Navigator.pushNamed(context, PersonalProfileScreen.routeName),
+              ),
+            ],
+          )),
+    );
+  }
+}
