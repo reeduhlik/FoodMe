eb4fa8406d7f6ecf5c703d68082e2b2fc4c52b26
cmeuse <chloemeuse@gmail.com>
2023-03-21 15:10:13 -0400

    chloe - provider widget outline implemented

--- lib/screens/provider_home/provider_home_screen.dart
+++ lib/screens/provider_home/provider_home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gsc2023_food_app/components/provider_nav_bar.dart';
-import '../../components/personal_nav_bar.dart';
+import '../../components/provider_listings_bar.dart';
+import '../../components/provider_nav_bar.dart';
+import '../../constants.dart';
 import '../../enums.dart';
 
 import 'components/body.dart';
@@ -10,6 +11,13 @@ class ProviderHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Map View", style: headingStyle),
+        automaticallyImplyLeading: false, // <--- This will remove the back button
+        actions: <Widget>[
+          ProviderListingsBar(selectedMenu: ProviderListingsMenuState.map)
+        ],
+      ), 
       body: Body(),
       bottomNavigationBar: ProviderNavBar(selectedMenu: ProviderMenuState.home),
     );
